e17be2294e7e0d225c733c725a4bed47cf2f67b4
Nimeka1206 <nimeshkakumudumali1206@gmail.com>
2026-04-26 20:59:29 +0530

    1699_Interface

---
+++ Interface_1699/IAttendanceViewer.java
@@ -0,0 +1,16 @@
+package ums.Interfaces;
+
+public interface IAttendanceViewer {
+    void viewAttendanceIndividual();
+    void viewAttendanceWholeBatch();
+    void viewAttendanceTheory();
+    void viewAttendancePractical();
+    void viewAttendancePracticaBoth();
+    void viewAttendanceEligibilityWithMedical();
+    void viewAttendanceNotEligibilityWithMedical();
+    void viewAttendanceEqualEligibility();
+    void viewAttendanceAboveEligibility();
+    void viewAttendanceNotEligibility();
+    void viewEligibilityIndividual();
+    void viewEligibilityWholeBatch();
+}
